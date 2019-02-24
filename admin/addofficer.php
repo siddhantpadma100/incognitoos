4f3f1c8ef5d35c718b60f4d21d773ba884226295
satish404 <imsatish1998@gmail.com>
2019-02-24 15:49:47 +0530

    satish

--- admin/addofficer.php
+++ admin/addofficer.php
@@ -9,7 +9,7 @@ if($_SESSION['admin_log']==1)
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Add Officer</title>
         <link href="../lib/css/dashboard.css" rel="stylesheet" id="bootstrap-css">
       
       <script src="lib/jquery-3.2.1.min.js">
