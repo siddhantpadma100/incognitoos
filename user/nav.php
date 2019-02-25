feb7637b26532abffc1b3c86ba05cd70ed52c8ad
siddhantpadma100 <siddhantpadma@gmail.com>
2019-02-25 14:18:31 +0530

    hii

--- user/nav.php
+++ user/nav.php
@@ -1,44 +1,4 @@
-<<<<<<< HEAD
-<nav class="navbar navbar-default navbar-static-top">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">
-              User
-            </a>
-        </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      
-           
-            <ul class="nav navbar-nav navbar-right">
-                
-                <li class="dropdown ">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                        Menu
-                        <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Change password</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Change profile</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Logout</a></li>
-                            <li class="divider"></li>                            
-                            
-                        </ul>
-                    </li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-    </nav> 
-
-=======
   <!--Navbar Start -->
   <div style="background-color:#33BEFF; text-align: center">
                 <div class="row">
@@ -66,6 +26,3 @@
                 </div>
             </div>
             <br>
-    
-  <!-- Navbar End -->
->>>>>>> ad8868d678bc68c147a7065cc757e23d5f0fbd76
--- user/sidebar.php
+++ user/sidebar.php
@@ -17,15 +17,11 @@
                             </div>
                         </div>
                     </li>
-<<<<<<< HEAD
-                    <li><a href="../grievance/register.php">Grievance</a></li>
-                    <li><a href="#">Notifications</a></li>
-                    
-=======
-                    <li><a href="closing.jsp">Grievance</a></li>
+
+                     <li><a href="../grievance/register.php">Grievance</a></li>
                     <li><a href="gen.jsp">Notifications</a></li>
                     <li><a href="logout.php">Logout</a></li>
->>>>>>> ad8868d678bc68c147a7065cc757e23d5f0fbd76
+
 
                 </ul>
             </div><!-- /.navbar-collapse -->
